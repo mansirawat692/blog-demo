30702dccd9a6d5d4377bd71c8827bdc19cd7d1cd
Mansi <mansirawat692@gmail.com>
2021-09-21 16:41:26 +0530

    frontend setup

---
+++ frontend/src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OverviewComponent } from './components/overview/overview.component';
+
+
+
+@NgModule({
+  declarations: [
+    OverviewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
